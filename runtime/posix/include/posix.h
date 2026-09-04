1fb0f6a8343d072a83341718a14014f60274c03e
abdelhalimyasser <abdelhalimyasser88@gmail.com>
2026-09-04 12:52:36 +0300

    ci: add native ARM64 and x86_64 platform matrix

--- runtime/posix/include/posix.h
+++ runtime/posix/include/posix.h
@@ -47,7 +47,7 @@ int lettuce_clock_gettime(clockid_t clk_id, struct timespec *tp);
 int lettuce_nanosleep(const struct timespec *req, struct timespec *rem);
 
 #ifndef LETTUCE_NO_POSIX_ALIASES
-#define write lettuce_write وتحت الغطا Lettuce بيحوّلها لـ sys
+#define write lettuce_write
 #define read lettuce_read
 #define close lettuce_close
 #define getpid lettuce_getpid
--- scripts/test.sh
+++ scripts/test.sh
@@ -2,15 +2,12 @@
 
 set -euo pipefail
 
-cmake -S . -B build
-cmake --build build
-./build/capability_unit
-./build/capability_security
-./build/same_layer_unit
-./build/cross_layer_unit
-./build/elevator_unit
-./build/memory_unit
-./build/context_nested_unit
-./build/dynamic_array_unit
-./build/task_scheduler_unit
-./build/posix_unit
+cmake -S . -B build \
+    -DCMAKE_BUILD_TYPE=Debug \
+    -DBUILD_TESTING=ON
+
+cmake --build build --parallel
+
+ctest \
+    --test-dir build \
+    --output-on-failure
